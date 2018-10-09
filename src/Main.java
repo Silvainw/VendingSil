272d240aff85959126909f14f7ec4f2797408370
Roy dupon <www.duponroy@gmail.com>
2018-10-09 11:28:24 +0200

    Nummers voor de array zodat je niet meer de prijs hoeft in te typen

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,34 @@ import java.util.Scanner;
 public class Main {
 
     public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
 //arraylist aangemaakt
     public static ArrayList<String> ProdID =new ArrayList<String>();
-    public static ArrayList<String> Cost  = new ArrayList<String>();
+
+
+    public void reset(){
+
+      ProdID = new ArrayList<String>();
+
+    }
 
     public static void main(String[] args) {
 
 
         Scanner keyboard = new Scanner(System.in);
 
-        int id;{
+        int id;
+        {
 
 //Producten geadd aan de arraylist
             ProdID.add("Broodje frikandel € 2,15");
             ProdID.add("Snicker € 1,-");
-            ProdID.add("Mars € 1,-");
-            ProdID.add("Cola € 1,10");
+            ProdID.add("Mars € 1,05-");
+            ProdID.add("Cola € 1,20");
             ProdID.add("Fanta € 1,10");
         }
+
 
         String answer;
         do {
@@ -34,11 +43,11 @@ public class Main {
 
 //print de arraylist
             for (int i = 0; i < ProdID.size(); i++) {
-                System.out.println(ProdID.get(i));
+                System.out.println(i + 1 + ". " + ProdID.get(i));
             }
 
-
-            System.out.println("Wat wilt u bestellen? :");
+            int budget = 15;
+            System.out.println("Wat wilt u bestellen? uw bedget is €" + budget);
             double price = keyboard.nextDouble();
 
             // vermedigvuldigd de prijs met het aantal artikelen
@@ -48,21 +57,25 @@ public class Main {
             double total = price * qty;
 
 //print de totale kosten die berekend zijn
-            System.out.printf("Totale kosten : $%.2f\n", total);
+            System.out.printf("Totale kosten : €%.2f\n", total);
 
-            if (total > 15){
 
-                System.out.println(ANSI_RED + "U heeft te weinig saldo!" );
+            if (total > 15) {
+//stopt met executen als het bedrag boven het budget is
+                System.out.println(ANSI_RED + "U heeft te weinig saldo!");
+                System.out.println("Neem uw geld terug." + ANSI_RESET);
+                System.exit(1);
+
 
             }
 
             System.out.print("Werp het geld nu in.\n");
 
             System.out.println("wilt u nog iet kopen? Ja of Nee");
-          answer = keyboard.next();
+            answer = keyboard.next();
         }
         //zorgt ervoor dat als je Ja invoerd dat de do opnieuw start
-            while (answer.equals("Ja"));
+        while (answer.equals("Ja")) ;
     }
 
 }
