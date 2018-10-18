a70f9077dad8c3269985acdfd36604f5d8acc5e6
Roy dupon <www.duponroy@gmail.com>
2018-10-18 20:05:48 +0200

    Meer uitkomsten toegevoegd plus de antwoorden worden opgeslagen in een array. En ook later uitgeprint + je balance gaat nu ook omlaag als je nog wat wilt (balance slaat nu op).

--- src/Main.java
+++ src/Main.java
@@ -10,30 +10,32 @@ public class Main {
     public static HashMap<String, Double> items = new HashMap<String, Double>();
     public static ArrayList<String> ProdID =new ArrayList<String>();
     public static ArrayList<String> ProdDrinken =new ArrayList<String>();
+    public static ArrayList<String> Bestelling =new ArrayList<String>();
+
+    public Scanner sc = new Scanner(System.in);
+    public double[] cost = {2.15, 1.00, 1.00, 1.10, 1.10};
+    public int id;
+    public String answer;
+    public double budget = 15;
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        double[] cost = {2.15, 1.00, 1.00, 1.10, 1.10};
+        Main main = new Main();
+        main.start();
 
+    }
 
+    public void start() {
 
+        //Producten geadd aan de arraylist
+        ProdID.add("Broodje frikandel €" + cost[0] );
+        ProdID.add("Snicker €" + cost[1] );
+        ProdID.add("Mars €" + cost[2] );
+        ProdDrinken.add("Cola €" + cost[3] );
+        ProdDrinken.add("Fanta €" + cost[4] );
 
-        int id;
-        {
-
-//Producten geadd aan de arraylist
-            ProdID.add("Broodje frikandel" + " " + " €" + cost[0] );
-            ProdID.add("Snicker" + " " + " €" + cost[1] );
-            ProdID.add("Mars €" + cost[2] );
-            ProdDrinken.add("Cola" + " " + " €" + cost[3] );
-            ProdDrinken.add("Fanta" + " " + " €" + cost[4] );
-        }
-
-
-        String answer;
         do {
-//print de eerste zinnetjes
+            //print de eerste zinnetjes
             System.out.println("Goedendag\n");
             System.out.println("Wilt u wat eten of drinken?");
             System.out.println("1.Eten  2.Drinken");
@@ -46,49 +48,62 @@ public class Main {
             System.out.println("Dit zijn onze artikelen: ");
 
 
-//print de arraylist eten
+            //print de arraylist eten
             if (EtenDrinken == 1) {
                 for (int i = 0; i < ProdID.size(); i++) {
                     System.out.println(i + 1 + ". " + ProdID.get(i));
                 }
             }
-//print de arraylist drinken
+            //print de arraylist drinken
             if (EtenDrinken == 2) {
                 for (int i = 0; i < ProdDrinken.size(); i++) {
                     System.out.println(i + 1 + ". " + ProdDrinken.get(i));
                 }
             }
-            double budget = 15;
             System.out.println("Wat wilt u bestellen? uw bedget is €" + budget);
 
             int choice = sc.nextInt();
             switch (choice) {
 
                 case 1:
-                    budget = 15-cost[0];
+                    budget -= cost[0];
                     System.out.println("Uw nieuwe budget is " + "€" + budget);
                     //print de totale kosten die berekend zijn
                     System.out.printf("Totale kosten : €%.2f\n", cost[0]);
                     System.out.print("Werp het geld nu in.\n");
-                    System.out.println("----------");
-                    System.out.println("(=========)");
-                    System.out.println("----------");
 
-
+                if (EtenDrinken == 1) {
+                    Bestelling.add("----------");
+                    Bestelling.add("(=========)");
+                    Bestelling.add("----------\n");
+                }
+                if (EtenDrinken == 2) {
+                        Bestelling.add("(----------)");
+                        Bestelling.add("|----------|");
+                        Bestelling.add("|----------|");
+                        Bestelling.add("(----------)\n");
+                }
                 break;
 
                 case 2:
-                budget = 15-cost[1];
-                System.out.println("Uw nieuwe budget is " + "€" + budget);
+                    budget -= cost[1];
+                    System.out.println("Uw nieuwe budget is " + "€" + budget);
                     //print de totale kosten die berekend zijn
                     System.out.printf("Totale kosten : €%.2f\n", cost[1]);
                     System.out.print("Werp het geld nu in.\n");
-                break;
+                    break;
 
+                case 3:
+                    budget -= cost[2];
+                    System.out.println("Uw nieuwe budget is " + "€" + budget);
+                    //print de totale kosten die berekend zijn
+                    System.out.printf("Totale kosten : €%.2f\n", cost[2]);
+                    System.out.print("Werp het geld nu in.\n");
+                    break;
             }
 
             if (budget < 0) {
-//stopt met executen als het bedrag boven het budget is
+                //stopt met executen als het bedrag boven het budget is
                 System.out.println(ANSI_RED + "U heeft te weinig saldo!");
                 System.out.println("Neem uw geld terug." + ANSI_RESET);
                 System.exit(1);
@@ -99,10 +114,17 @@ public class Main {
             answer = sc.next();
         }
         //zorgt ervoor dat als je Ja invoerd dat de do opnieuw start
-        while (answer.equals("Ja")) ;
+        while (answer.equalsIgnoreCase("ja")) ;
+        if (answer.equalsIgnoreCase("nee")) {
+
+            Bestelling.forEach(System.out::println);
+            System.exit(1);
+        }
+
+        }
+
     }
 
-}
 
 
 
